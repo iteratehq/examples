2740c2c47904ddfb47663b8a292178e2ec21c7c8
Mike Singleton <mike@iteratehq.com>
2023-01-16 15:20:26 -0500

    Add carthage example

---
+++ ios/Carthage/Carthage/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Carthage
+//
+//  Created by Mike Singleton on 1/16/23.
+//
+
+import SwiftUI
+import IterateSDK
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Button("Show survey") {
+                Iterate.shared.preview(surveyId: "5efa0121a9fffa0001c70b8d")
+                Iterate.shared.sendEvent(name: "show-survey-button-tapped")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
