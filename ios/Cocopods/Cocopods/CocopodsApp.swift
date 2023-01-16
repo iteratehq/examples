a9dcfc24610f7780ee8f006e14496f057068a2dd
Mike Singleton <mike@iteratehq.com>
2023-01-16 14:41:02 -0500

    Add cocopods example

---
+++ ios/Cocopods/Cocopods/CocopodsApp.swift
@@ -0,0 +1,27 @@
+//
+//  CocopodsApp.swift
+//  Cocopods
+//
+//  Created by Mike Singleton on 1/16/23.
+//
+
+import SwiftUI
+import IterateSDK
+
+@main
+struct CocopodsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Iterate.shared.configure(apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55X2lkIjoiNWRmZTM2OGEwOWI2ZWYwMDAxYjNlNjE4IiwiaWF0IjoxNTc2OTQxMTk0fQ.QBWr2goMwOngVhi6wY9sdFAKEvBGmn-JRDKstVMFh6M")
+        return true
+    }
+}
