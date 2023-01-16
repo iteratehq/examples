4560ea4af95ebc645b1e646534d688a5ad093ead
Mike Singleton <mike@iteratehq.com>
2023-01-16 15:53:12 -0500

    Add swift package manager example

---
+++ ios/SwiftPackageManager/SwiftPackageManager/SwiftPackageManagerApp.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftPackageManagerApp.swift
+//  SwiftPackageManager
+//
+//  Created by Mike Singleton on 1/16/23.
+//
+
+import SwiftUI
+import Iterate
+
+@main
+struct SwiftPackageManagerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            Iterate.shared.configure(apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb21wYW55X2lkIjoiNWRmZTM2OGEwOWI2ZWYwMDAxYjNlNjE4IiwiaWF0IjoxNTc2OTQxMTk0fQ.QBWr2goMwOngVhi6wY9sdFAKEvBGmn-JRDKstVMFh6M")
+            return true
+        }
+    }   
+}
